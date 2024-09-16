1565e643510adce64afca5266baf6e1ef71a968d
KaioSiq <kaio.siqueira@poli.ufrj.br>
2024-09-16 02:12:55 -0300

    workflow fix

--- poke_api_producer/test/mock_poke_api_client.py
+++ poke_api_producer/test/mock_poke_api_client.py
@@ -2,7 +2,14 @@ import pathlib
 import json
 from ..entities.pokemon import Pokemon
 
+
+from pathlib import Path
+
+# Get the current file directory
+current_dir = Path(__file__).parent.resolve()
+
+
 class MockPokeApiClient:
     def get_pokemon_by_id(self, id: int):
-        response = json.loads(pathlib.Path("poke_api_producer/test/api_return_value.json").read_text())
+        response = json.loads(Path(current_dir/"api_return_value.json").read_text())
         return Pokemon.model_validate(response) 
